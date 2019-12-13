fdb4aabe690fae8bcd559d209c7ab3f181ddf9a8
Radon Rosborough <radon.neon@gmail.com>
2019-12-12 19:22:48 -0800

    Add skeleton dumbparens.el

---
+++ dumbparens.el
@@ -0,0 +1,35 @@
+;;; dumbparens.el --- Simple paren-matching solution -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Created: 12 Dec 2019
+;; Homepage: https://github.com/raxod502/dumbparens
+;; Keywords: extensions
+;; Package-Requires: ((emacs "25.1"))
+;; Version: 0
+
+;;; Commentary:
+
+;; Please see https://github.com/raxod502/dumbparens for more
+;; information.
+
+;;; Code:
+
+;; To see the outline of this file, run M-x outline-minor-mode and
+;; then press C-c @ C-t. To also show the top-level functions and
+;; variable declarations in each section, run M-x occur with the
+;; following query: ^;;;;* \|^(
+
+
+
+;;;; Closing remarks
+
+(provide 'dumbparens)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; outline-regexp: ";;;;* "
+;; End:
+
+;;; dumbparens.el ends here
